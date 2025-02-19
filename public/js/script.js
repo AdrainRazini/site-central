d83f51bbccca0cee760ebf880ec10918b47aa873
AdrainRazini <160901383+AdrainRazini@users.noreply.github.com>
2025-02-19 15:29:11 -0300

    de

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,1 @@
-
-        AOS.init();
-        function toggleModo() {
-            document.body.classList.toggle("dark-mode");
-            let icon = document.querySelector(".modo-escuro");
-            icon.classList.toggle("fa-sun");
-            icon.classList.toggle("fa-moon");
-        }
-
-        function toggleMenu() {
-            document.querySelector(".nav-links").classList.toggle("active");
-        }
-
-        gsap.from(".menu ul li", { opacity: 0, y: -50, duration: 1, stagger: 0.2 });
-        gsap.from(".btn", { opacity: 0, scale: 0.5, duration: 1, stagger: 0.3 });
-  
+88
