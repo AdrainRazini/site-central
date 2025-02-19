7392c8a435ba8fc006bc1821a2ecc49e32ab4998
AdrainRazini <160901383+AdrainRazini@users.noreply.github.com>
2025-02-19 18:33:01 -0300

    arrumei o modal

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,12 @@
-function toggleModo() {
-    document.body.classList.toggle("dark-mode");
-    let icon = document.querySelector(".modo-escuro");
-    icon.classList.toggle("fa-sun");
-    icon.classList.toggle("fa-moon");
-}
-
 function openModal(title, description, img, link) {
+    if (!title || !description || !img || !link) {
+        console.error("Dados do modal não foram fornecidos corretamente.");
+        return;
+    }
+
     document.getElementById("modal-title").innerText = title;
     document.getElementById("modal-description").innerText = description;
     document.getElementById("modal-img").src = img;
     document.getElementById("modal-link").href = link;
     document.getElementById("modal").style.display = "flex";
-}
-
-function closeModal() {
-    document.getElementById("modal").style.display = "none";
-}
-
-function toggleMenu() {
-    document.querySelector(".nav-links").classList.toggle("active");
 }
